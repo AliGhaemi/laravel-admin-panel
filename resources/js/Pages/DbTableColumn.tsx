5560e676a38baedc7a184600cda679cba7bd7ed1
ali ghaemi <alighaemi181@gmail.com>
2025-08-26 22:04:08 +0330

    created state for columns and columnRows

--- resources/js/Pages/DbTableColumn.tsx
+++ resources/js/Pages/DbTableColumn.tsx
@@ -1,36 +1,62 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
-export default function DbTableColumn({columns, columnRows}) {
-    console.log(columns)
-    console.log(columnRows)
+type columnsType = {
+    [key: string]: any;
+};
+type columnRowsType = {
+    [key: string]: any;
+};
+
+export default function DbTableColumn(props) {
+
+    const [columns, setColumns] = useState<columnsType[]>(props.columns);
+    const [columnRows, setColumnRows] = useState<columnRowsType[]>(props.columnRows);
+    const [isLoading, setIsLoading] = useState(true);
+
+    // setColumnRows(props.columnRows)
+    // console.log(props.columns)
+    // console.log(props.columnRows)
+
+    useEffect(() => {
+        const filteredColumns = columns.filter(item => item !== "id");
+        setColumns(["id", ...filteredColumns])
+        setIsLoading(false);
+    }, [])
+
     return (
-        <table className="table-auto">
-            <thead className="uppercase">
-            <tr>
-                {columns.map((column, index) => (
-                    <th scope="col" className="px-6 py-3 text-left">{column}</th>
-                ))}
-                <th scope="col" className="px-6 py-3 text-left ml-auto">Actions</th>
-            </tr>
-            </thead>
+        <>
+            {columns && columns.length > 0 ? (
+                <table className="table-auto h-28">
+                    <thead className="uppercase">
+                    <tr>
+                        {columns.map((column, index) => (
+                            <th key={index} scope="col" className="px-6 py-3 text-left">{column}</th>
+                        ))}
+                        <th scope="col" className="px-6 py-3 text-left ml-auto">Actions</th>
+                    </tr>
+                    </thead>
 
-            <tbody>
-            {columnRows.data.map((row, index) => (
-                <tr key={index} >
-                    <th scope="row">{row.id}</th>
-                    {delete row.id}
-                    {
-                        Object.values(row).map((rowItem, index) => (
-                            <td className="px-6 py-4">{rowItem}</td>
-                        ))
-                    }
-                    <td className="px-6 py-4 grid grid-cols-2 gap-3 w-72 text-primary font-bold">
-                        <p className="bg-sky-400 rounded-md px-4 py-2 hover:bg-hover hover:cursor-pointer">Edit</p>
-                        <p className="bg-red-400 rounded-md px-4 py-2 hover:bg-hover hover:cursor-pointer">Delete</p>
-                    </td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
+                    <tbody>
+                    {columnRows.data.map((row, index) => (
+                        <tr key={index}>
+                            {row.id == "id" ?
+                                <th scope="row">{row.id}</th>
+                                :
+                                Object.values(row).map((rowItem, index) => (
+                                    <td key={index} className="px-6 py-4">{rowItem}</td>
+                                ))
+                            }
+                            <td className="px-6 py-4 grid grid-cols-2 gap-3 w-72 text-primary font-bold">
+                                <p className="bg-sky-400 rounded-md px-4 py-2 hover:bg-hover hover:cursor-pointer">Edit</p>
+                                <p className="bg-red-400 rounded-md px-4 py-2 hover:bg-hover hover:cursor-pointer">Delete</p>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </>
     );
 }
