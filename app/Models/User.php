0c606ab9eedea10b301948755cb348a2a34438a9
ali ghaemi <alighaemi181@gmail.com>
2025-09-03 16:19:27 +0330

    Added isAdmin() method

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,4 +66,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(AdminAccessToken::class);
     }
+
+    public function isAdmin(): bool
+    {
+        return $this->is_admin;
+    }
 }
