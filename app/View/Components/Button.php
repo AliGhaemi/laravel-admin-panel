258212d1551cafbdd4a2309f6406fc3fffe57534
ali ghaemi <alighaemi181@gmail.com>
2025-09-04 16:31:19 +0330

    Button component Added

---
+++ app/View/Components/Button.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Button extends Component
+{
+    public $text;
+    public function __construct($text)
+    {
+        $this->text = $text;
+    }
+    public function render(): View|Closure|string
+    {
+        return view('components.button');
+    }
+}
