8bc632f38db31ed5b7f41ae8db3380afb1f903f5
ali ghaemi <alighaemi181@gmail.com>
2025-09-04 16:43:53 +0330

    Added anchor to main page

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,4 +1,7 @@
 <header class="h-20 px-4 w-full fixed top-0 bg-secondary flex items-center justify-end">
+    <a href="/">
+        <img class="mr-6 w-12 h-12" src="{{ Vite::index('favicon.svg') }}" alt="Home Icon">
+    </a>
     @if(Auth::check())
         <div class="mr-auto flex flex-row items-center gap-4">
             <img class="rounded-3xl w-12 h-12"
