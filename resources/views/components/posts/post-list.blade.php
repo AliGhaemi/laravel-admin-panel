3e4a32f2d7a92e6f88c1e7ba75cfc9e775ff1639
ali ghaemi <alighaemi181@gmail.com>
2025-09-04 16:35:39 +0330

    Conditional for factory created images (static links) and user created images (dynamic links)

--- resources/views/components/posts/post-list.blade.php
+++ resources/views/components/posts/post-list.blade.php
@@ -6,11 +6,16 @@
         <ul class="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
             @foreach($posts as $post)
                 <li class="flex flex-col gap-4 items-center text-center rounded-3xl bg-secondary overflow-hidden">
-                    <img class="w-full h-35" alt="1" src="{{ $post->image_path }}">
+                    @if(Str::contains($post->image_path,'picsum.photos'))
+                        <img class="w-full h-35" alt="1" src="{{ $post->image_path }}">
+                    @else
+                        <img class="w-full h-35" alt="1" src="{{ asset('storage/'. $post->image_path) }}">
+                    @endif
                     <h3 class="text-md mx-1">{{ $post->title }}</h3>
                     <p class="text-sm mx-3 mb-4">{{ Str::limit($post->description, $limit) }}</p>
                     <a type="button" href="{{ route('posts.show', ['post' => $post]) }}"
-                       class="w-full p-3 bg-utility mt-auto flex justify-center items-center hover:bg-hover duration-200 hover:ease-in-out hover:cursor-pointer">Read More</a>
+                       class="w-full p-3 bg-utility mt-auto flex justify-center items-center hover:bg-hover duration-200 hover:ease-in-out hover:cursor-pointer">Read
+                        More</a>
                 </li>
             @endforeach
         </ul>
