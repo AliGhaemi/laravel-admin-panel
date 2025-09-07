2ab82f53188db78da293f14cc080a3bb3b12cece
ali ghaemi <alighaemi181@gmail.com>
2025-09-07 23:41:38 +0330

    Added store method in CommentController.php | Modified showReplies method with eager loading the count

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,15 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentRequest;
 use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class CommentController extends Controller
 {
-    public function showReplies(Comment $comment)
+    public function showReplies(Post $post, Comment $comment)
     {
-        $replies = $comment->replies()->with('user')->get();
+        $replies = $comment->replies()->with('user')->withCount('replies')->get();
+        return view('posts.replies', compact('replies', 'post'));
+    }
 
-        return view('posts.replies', compact('replies'));
+    public function store(CommentRequest $request, Post $post, Comment $comment)
+    {
+        $post->comments()->create([
+            'body' => $request->validated()['comment-body'],
+            'user_id' => Auth::id(),
+            'parent_id' => $request->validated()['parent_id'] ?? null
+        ]);
+        return Redirect::back()->with('success', 'Comment created.');
     }
 }
