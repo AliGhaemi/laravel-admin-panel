4ad10da189bdb7cd11a8e558fa5d08fa0683626a
ali ghaemi <alighaemi181@gmail.com>
2025-09-07 23:43:35 +0330

    Modified posts.replies and posts.show

--- resources/views/posts/replies.blade.php
+++ resources/views/posts/replies.blade.php
@@ -1,6 +1,6 @@
 <ul>
     @foreach ($replies as $reply)
-        <li class="flex flex-col gap-5 mx-20 py-6 border border-x-0 border-t-0 border-b-utility">
+        <li class="flex flex-col gap-5 mx-5 py-6 border border-x-0 border-t-0 border-b-utility last:border-b-0">
             <div class="mr-auto flex flex-row items-center gap-4">
                 <img class="rounded-3xl w-8 h-8"
                      src="{{ asset('storage/'. $reply->user->picture_path) }}"
@@ -10,9 +10,24 @@
             <div class="">
                 <div class="">
                     <p class="text-xl block">{{ $reply->body }}</p>
-                    <x-button onclick="loadReplies({{ $reply->id }})" text="Load Replies"/>
+                    <div class="flex flex-row gap-5 mt-4">
+                        @if($reply->replies_count > 0)
+                            <p onclick="loadReplies({{ $reply->id }}, {{ $post->id }})"
+                               class="cursor-pointer hover:text-blue-500 border border-l-0 border-y-0 border-r-utility pr-5">Load Replies</p>
+                        @endif
+                        <p onclick="openReplySection({{ $reply->id }})" class="cursor-pointer hover:text-blue-500">Reply to
+                            this comment</p>
+                    </div>
                 </div>
                 <div class="mx-8" id="replies-{{ $reply->id }}"></div>
+                <form id="reply-form-{{ $reply->id }}" action="{{ route('comments.store',$post) }}" method="POST"
+                      class="hidden flex-col gap-4 px-14">
+                    @csrf
+                    <x-form-field id="comment-body" type="textarea" label="Write A Comment"
+                                  value="{{ old('comments-body') }}"/>
+                    <input type="hidden" name="parent_id" value="{{ $reply->id }}">
+                    <x-button type="submit" text="Submit" class="w-40 ml-auto"/>
+                </form>
             </div>
         </li>
     @endforeach
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -31,37 +31,74 @@
             {!! wordwrap($post->description, 200, '<br><br>') !!}
         </p>
     </div>
-    <div class="px-5">
+    <div class="mx-5 py-5 border border-x-0 border-b-0 border-t-utility">
+        <form action="{{ route('comments.store', $post) }}" method="POST"
+              class="flex flex-col gap-4 px-14">
+            @csrf
+            <x-form-field id="comment-body" type="textarea" label="Write A Comment"
+                          value="{{ old('comments-body') }}"/>
+            <x-button type="submit" text="Submit" class="w-40 ml-auto"/>
+        </form>
         <h1 class="text-4xl my-6 border border-x-0 border-t-0 border-b-utility py-4">Comments</h1>
-        <ul>
-            @foreach($post->comments as $comment)
-                <li class="flex flex-col gap-5 mx-20 py-6 border border-x-0 border-t-0 border-b-utility">
-                    <div class="mr-auto flex flex-row items-center gap-4">
-                        <img class="rounded-3xl w-8 h-8"
-                             src="{{ asset('storage/'. $comment->user->picture_path) }}"
-                             alt="{{ $comment->user->username }}">
-                        <p>From {{ $comment->user->username }}</p>
-                    </div>
-                    <div class="">
-                        <div class="">
-                            <p class="text-xl block">{{ $comment->body }}</p>
-                            <x-button onclick="loadReplies({{ $comment->id }})" text="Load Replies"/>
+        @if($post->comments->isNotEmpty())
+            <ul>
+                @foreach($post->comments as $comment)
+                    <li class="flex flex-col gap-5 mx-20 py-6 border border-x-0 border-t-0 border-b-utility  last:border-b-0">
+                        <div class="mr-auto flex flex-row items-center gap-4">
+                            <img class="rounded-3xl w-8 h-8"
+                                 src="{{ asset('storage/'. $comment->user->picture_path) }}"
+                                 alt="{{ $comment->user->username }}">
+                            <p>From {{ $comment->user->username }}</p>
                         </div>
-                        <div class="mx-8" id="replies-{{ $comment->id }}"></div>
-                    </div>
-                </li>
-            @endforeach
-        </ul>
+                        <div>
+                            <div>
+                                <p class="text-xl block">{{ $comment->body }}</p>
+                                <div class="flex flex-row gap-5 mt-4">
+                                    @if($comment->replies_count > 0)
+                                        <p onclick="loadReplies({{ $comment->id }}, {{ $post->id }})"
+                                           class="cursor-pointer hover:text-blue-500 border border-l-0 border-y-0 border-r-utility pr-5">Load Replies</p>
+                                    @endif
+                                    <p onclick="openCommentSection({{ $comment->id }})" class="cursor-pointer hover:text-blue-500">Reply to
+                                        this comment</p>
+                                </div>
+                            </div>
+                            <div class="mx-8" id="replies-{{ $comment->id }}"></div>
+                            <form id="comment-form-{{ $comment->id }}" action="{{ route('comments.store',$post) }}"
+                                  method="POST"
+                                  class="hidden flex-col gap-4 px-14">
+                                @csrf
+                                <x-form-field id="comment-body" type="textarea" label="Write A Comment"
+                                              value="{{ old('comments-body') }}"/>
+                                <input type="hidden" name="parent_id" value="{{ $comment->id }}">
+                                <x-button type="submit" text="Submit" class="w-40 ml-auto"/>
+                            </form>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <h1 class="text-4xl">
+                No Comments Here! Be The First One To Comment On This Post.
+            </h1>
+        @endif
     </div>
     <script>
-        function loadReplies(commentId) {
+        function loadReplies(commentId, postId) {
             const replyElement = document.getElementById('replies-' + commentId)
-            fetch(`/comments/${commentId}/replies`)
+            fetch(`/${postId}/${commentId}/replies`)
                 .then(response => response.text())
                 .then(view => {
                     replyElement.innerHTML = view;
                 })
                 .catch(e => console.error(e));
+        }
+
+        function openCommentSection(commentId) {
+            document.getElementById('comment-form-' + commentId).classList.replace('hidden', 'flex')
+        }
+
+        function openReplySection(replyId) {
+            document.getElementById('reply-form-' + replyId).classList.replace('hidden', 'flex')
         }
     </script>
 </x-layouts.layout>
