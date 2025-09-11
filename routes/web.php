ff515d51a076e6edb9b110bf6a2bb1ed5ee08c4c
ali ghaemi <alighaemi181@gmail.com>
2025-09-11 19:02:23 +0330

    Added lang route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\DatabaseOperatorsController;
+use App\Http\Controllers\LocalizationController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\SendRequestController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
+Route::get('{lang}', LocalizationController::class)->name('lang');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Home');
@@ -16,3 +19,4 @@ Route::get('/search', [PostController::class, 'search'])->name('posts.search');
 
 Route::get('/send-request', [SendRequestController::class, 'create'])->name('send.request.create');
 Route::post('/send-request', [SendRequestController::class, 'store'])->name('send.request.store');
+
