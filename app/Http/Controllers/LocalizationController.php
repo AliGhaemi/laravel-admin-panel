000f2e0d074f2b3091556e1f6b69c8824e8cec76
ali ghaemi <alighaemi181@gmail.com>
2025-09-11 19:00:36 +0330

    Modified LocalizationController

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -11,9 +11,7 @@ class LocalizationController extends Controller
      */
     public function __invoke(Request $request, $lang)
     {
-        if (!in_array($lang, ['en', 'tr'])) {
-            abort(400);
-        }
+        !in_array($lang, ['en', 'tr']) ?? abort(400);
         session(['locale' => $lang]);
         return redirect()->back();
     }
